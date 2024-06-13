a41e89875164f7f2a0b8aa5bac822e37d118c9f3
shu-sun-yat-sen <shuchj@mail2.sysu.edu.cn>
2024-06-13 17:25:38 +0800

    merge=2024-6-13

--- src/server_side_demo/src/main/java/com/example/demo1/mapper/DbMapper/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo1.mapper.DbMapper;
-
-import com.example.demo1.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
