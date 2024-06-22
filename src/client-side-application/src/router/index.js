d7452bef0c7e4939516d1c36a1eb0a733e91de02
Amadeus <240971975@qq.com>
2024-06-22 16:06:05 +0800

    chess v1.0

--- src/client-side-application/src/router/index.js
+++ src/client-side-application/src/router/index.js
@@ -4,21 +4,47 @@ import App from '@/App.vue';
 import ChessGame from '@/components/inputWindowComponent/ChessGame.vue';
 
 
+// const routes = [
+//     {
+//         path: '/',
+//         name: 'App',
+//         component: App,
+//         meta: { 
+//             showExtraContent: true
+//         }
+//     },
+//     {
+//         path: '/ChessGame',
+//         name: 'ChessGame',
+//         component: ChessGame,
+//         meta: { showExtraContent: false },
+//         children :[
+//             {
+//                 path : 'child/:id',
+
+//             }
+//         ]
+//     }
+// ];
+
 const routes = [
     {
         path: '/',
         name: 'App',
         component: App,
-        meta: { showExtraContent: true }
-    },
-    {
-        path: '/ChessGame',
-        name: 'ChessGame',
-        component: ChessGame,
-        meta: { showExtraContent: false }
+        meta: { 
+            showExtraContent: true
+        },
+        children: [
+            {
+                path : 'ChessGame',
+                name: 'ChessGame',
+                component: ChessGame,
+                meta: { showExtraContent: false }
+            }
+        ]
     }
 ];
-
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
