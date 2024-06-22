76ba4a4cd4e63ae0f53d5342cc64453bef47f1a3
Debris <hzsp150383@gmail.com>
2024-06-22 15:24:23 +0800

    Add files via upload
    修改机器人对每个用户单独记忆

--- src/server2/src/main/java/com/example/demo1/Alservice/Assistant.java
+++ src/server2/src/main/java/com/example/demo1/Alservice/Assistant.java
@@ -1,12 +1,14 @@
 package com.example.demo1.Alservice;
 
-
+import dev.langchain4j.service.Moderate;
+import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 interface Assistant {
 
-    @SystemMessage("You are a polite assistant")
-    String chat(String userMessage);
+    @Moderate
+    @SystemMessage("You are a polite assistant in an anonymous chat room")
+    String chat(@MemoryId int memoryId,  String userMessage);
 }
