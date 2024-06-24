505ee3ba48a947f44d225f3a86c36a053b86058b
Amadeus <240971975@qq.com>
2024-06-24 19:17:30 +0800

    FrontEnd Pack

--- src/launchFrontEnd.bat
+++ src/launchFrontEnd.bat
@@ -5,12 +5,16 @@ setlocal
 :: 设置变量
 set MY_VAR=Hello, World!
 
-:: 新建一个终端窗口并在其中使用变量
-start cmd /k "cd client-side-application\src\components\inputWindowComponent\SnakeGame && npm run dev"
+REM 检查是否全局安装了http-server
+npm list -g --depth=0 | findstr /i "http-server"
 
-:: 在当前终端窗口中使用变量
-cd client-side-application
-npm run serve
-pause
-
+if %errorlevel% equ 0 (
+    :: 新建一个终端窗口并在其中使用变量
+    start cmd /k "cd FrontEndPackage\SnakeDist && http-server -p 8082"
+    :: 在当前终端窗口中使用变量
+    cd  FrontEndPackage\MainDist
+    http-server -p 8081
+) else (
+    echo http-server not install.
+)
 endlocal
